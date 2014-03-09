592bac30a95f2c217ce32122d96ef13a8b45eb02
Carlos Ledezma <cgledezma1101@gmail.com>
2014-03-09 16:34:04 -0430

    Developed models

--- discharger/models.py
+++ discharger/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class Discharge(models.Model)
+
+# Main object representing a discharging process
+class Discharge(models.Model):
   end_time = models.DateTimeField(null = True, blank = True)
   location = models.CharField(maxlength = 10)
   name = models.CharField(maxlength = 100)
+  stages = models.ManyToMany('Stage', through = 'PassedBy')
   start_time = models.DateTimeField()
+
+  def __unicode__(self):
+    return 'Patient: ' + self.name + ', Location: ' + self.location
+
+class Stage(models.Model):
+  description = TextField(null = True, blank = True)
+  discharges = models.ManyToMany('Discharge', through = 'PassedBy')
+  name = CharField(maxlength = 100)
+  sequence_number = IntegerField()
+
+  def __unicode__(self):
+    return self.name
+
+class PassedBy:
+  discharge = models.ForeignKey('Discharge')
+  entry_time = models.DateTimeField()
+  exit_time = models.DateTimeField()
+  stage = models.ForeignKey('Stage')
+
+  def __unicode__(self):
+    return self.discharge + ', Stage: ' + self.stage
