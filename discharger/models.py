3bcf1f4337eea4d17a59327447f91713af8a2cf9
Carlos Ledezma <cgledezma1101@gmail.com>
2014-03-09 17:25:29 -0430

    Finished developing models

--- discharger/models.py
+++ discharger/models.py
@@ -5,28 +5,39 @@ from django.db import models
 # Main object representing a discharging process
 class Discharge(models.Model):
   end_time = models.DateTimeField(null = True, blank = True)
-  location = models.CharField(maxlength = 10)
-  name = models.CharField(maxlength = 100)
-  stages = models.ManyToMany('Stage', through = 'PassedBy')
+  location = models.CharField(max_length = 10)
+  patient_name = models.CharField(max_length = 100)
   start_time = models.DateTimeField()
+  stages = models.ManyToManyField('Stage', through = 'PassedBy')
 
+  # Printed representation of the object
+  # return [String] The string representation of the object
   def __unicode__(self):
-    return 'Patient: ' + self.name + ', Location: ' + self.location
+    return 'Patient: ' + self.patient_name + ', Location: ' + self.location
 
-class Stage(models.Model):
-  description = TextField(null = True, blank = True)
-  discharges = models.ManyToMany('Discharge', through = 'PassedBy')
-  name = CharField(maxlength = 100)
-  sequence_number = IntegerField()
-
-  def __unicode__(self):
-    return self.name
-
-class PassedBy:
+# Join table that represents a stage through which a discharge process has
+# passed. If exit_time is null, then the discharge is still on this stage
+class PassedBy(models.Model):
   discharge = models.ForeignKey('Discharge')
   entry_time = models.DateTimeField()
-  exit_time = models.DateTimeField()
+  exit_time = models.DateTimeField(null = True, blank = True)
   stage = models.ForeignKey('Stage')
 
+  # Printed representation of the object
+  # return [String] The string representation of the object
   def __unicode__(self):
     return self.discharge + ', Stage: ' + self.stage
+
+# Stages through which a discharge process can passed. They are ordered by
+# a sequence number, which allows to tell, when a stage is done, which is the
+# next one(s) to come
+class Stage(models.Model):
+  description = models.TextField(null = True, blank = True)
+  discharges = models.ManyToManyField('Discharge', through = 'PassedBy')
+  name = models.CharField(max_length = 100)
+  sequence_number = models.IntegerField()
+
+  # Printed representation of the object
+  # return [String] The string representation of the object
+  def __unicode__(self):
+    return self.name
