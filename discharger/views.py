0033a24b92e9410f2f061fe027eeacba2065d9c9
Carlos Ledezma <cgledezma1101@gmail.com>
2014-03-17 19:50:56 -0430

    Forced ordering of the list

--- discharger/views.py
+++ discharger/views.py
@@ -4,6 +4,7 @@ from discharger.models import *
 # Create your views here.
 def discharge_list(request):
   # Retrieve all the discharges that haven't ended
-  discharges = Discharge.objects.filter(end_time__isnull = True)
+  discharges = Discharge.objects.filter(end_time__isnull = True) \
+                                .order_by('start_time')
   view_params = { 'discharges': discharges }
   return render(request, 'discharger/discharge_list.html', view_params)
