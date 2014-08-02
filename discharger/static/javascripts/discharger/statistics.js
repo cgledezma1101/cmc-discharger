ae279099820af8ec8849a3114f5cbb139dcc39d5
Carlos Ledezma <cgledezma1101@gmail.com>
2014-08-02 13:15:32 -0430

    Added some simple validation on the statistics search form

--- discharger/static/javascripts/discharger/statistics.js
+++ discharger/static/javascripts/discharger/statistics.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+  // This initializes the datepickers
   $('.datepicker').datepicker();
+
+  // Handle the submission of the search form
+  $('#search-submit').on('click', function(event){
+    event.preventDefault();
+
+    $('.form-errors').text("");
+    date_o = $('#initialDate').val();
+    date_f = $('#finalDate').val();
+
+    if(date_o == "" || date_f == ""){
+      $('.form-errors').text("Ambas fechas son obligatorias");
+      return false;
+    } else if($.datepicker.parseDate('mm/dd/yy', date_o) >=
+              $.datepicker.parseDate('mm/dd/yy', date_f)){
+      $('.form-errors').text("La fecha de fin debe ser después de la fecha " +
+                             "de inicio")
+    }
+  })
 })
